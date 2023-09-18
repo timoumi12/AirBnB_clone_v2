88d1559ec83bf1486952cf283d32c5450a791a53
Iheb Timoumi <55882164+timoumi12@users.noreply.github.com>
2023-09-18 12:54:48 +0100

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,12 +7,4 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
 
-CREATE UF NOT EXIST DATABASE hbnb_test_db;
-
-CREATE IF NOT EXIST USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost'
-
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
 FLUSH PRIVILEGES;
