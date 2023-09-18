f7a79e7f612f63dfdad6b37c7eb01b893e1495c4
asmarais <94301506+asmarais@users.noreply.github.com>
2023-09-18 12:56:49 +0100

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- a script that prepares a MySQL server for the project
 
-CREATE UF NOT EXIST DATABASE hbnb_test_db;
+CREATE IF NOT EXIST DATABASE hbnb_test_db;
 
 CREATE IF NOT EXIST USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
@@ -9,4 +9,3 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost'
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
-
