0d3b6ed087850c61f942bb04cb25cff539256173
Iheb Timoumi <55882164+timoumi12@users.noreply.github.com>
2023-09-18 12:55:37 +0100

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,12 @@
 -- a script that prepares a MySQL server for the project
-CREATE IF NOT EXIST DATABASE hbnb_dev_db;
 
-CREATE IF NOT EXIST USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE UF NOT EXIST DATABASE hbnb_test_db;
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
+CREATE IF NOT EXIST USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost'
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
+
