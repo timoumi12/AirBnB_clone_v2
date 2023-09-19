bea8d723a70639ec166c8dccf1fa40d9ad084b11
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-20 00:12:27 +0100

    fixing user 7th task

--- models/user.py
+++ models/user.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
