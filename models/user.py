cf4ebbe5b24113c8bdead5b142c202aa0ad97867
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-20 00:15:42 +0100

    back to original

--- models/user.py
+++ models/user.py
@@ -3,9 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
-from sqlalchemy.ext.declarative import declarative_base
 
 
 class User(BaseModel, Base):
@@ -15,7 +12,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
