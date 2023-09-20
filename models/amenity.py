c36efe861497e685da9f5ebc50b2068d7147f0a2
asmarais <94301506+asmarais@users.noreply.github.com>
2023-09-20 14:35:28 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,10 +3,11 @@
 from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
 from sqlalchemy import String, Column
-from models.place import Place
+from models.place import place_amenity
 
 
 class Amenity(BaseModel):
+    """ class Amenity inherits from BaseModel and Base """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary='place_amenity')
+    place_amenities = relationship("Place", secondary=place_amenity)
