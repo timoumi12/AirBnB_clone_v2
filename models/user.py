050cceebac158d305f303d65cbc52cdacda2bda5
asmarais <94301506+asmarais@users.noreply.github.com>
2023-09-20 11:14:26 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -10,5 +10,5 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True, default="NULL")
-    last_name = Column(String(128), nullable=True, default="NULL")
+    first_name = Column(String(128))
+    last_name = Column(String(128))
