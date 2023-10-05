c26821597f81e34bdaf7376397ee70cb4536ef24
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-05 18:11:44 +0100

    hot fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,21 +4,21 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-sudo printf %s "<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "51i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i "51i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
 
 sudo service nginx restart
