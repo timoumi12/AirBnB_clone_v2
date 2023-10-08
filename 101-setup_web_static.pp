3d67e9871535b70cd102c2e4c5b061680a810507
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-08 22:16:54 +0100

    advanced

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,45 +1,47 @@
 # sets up your web servers for the deployment of web_static
 exec { 'update':
   provider => shell,
-  command  => 'sudo apt-get -y update',
+  command  => 'sudo apt -y update',
   before   => Exec['install nginx']
 }
-exec { 'install nginx'
+exec { 'install nginx':
   provider => shell,
-  command  => 'sudo apt-get -y install nginx',
+  command  => 'sudo apt -y install nginx',
   before   => Exec['create /data/web_static/shared/']
 }
-exec { 'create /data/web_static/shared/'
+exec { 'create /data/web_static/shared/':
   provider => shell,
-  command => 'sudo mkdir -p /data/web_static/shared/'
+  command  => 'sudo mkdir -p /data/web_static/shared/',
   before   => Exec['create /data/web_static/releases/test/']
 }
-exec { 'create /data/web_static/releases/test/'
+exec { 'create /data/web_static/releases/test/':
   provider => shell,
-  command => 'sudo mkdir -p /data/web_static/releases/test/'
+  command  => 'sudo mkdir -p /data/web_static/releases/test/',
   before   => Exec['add test content']
 }
-exec { 'add test content'
+exec { 'add test content':
   provider => shell,
-  command => 'echo "Hello World!" > sudo tee /data/web_static/releases/test/index.html
+  command  => 'echo "Hello World!" > sudo tee /data/web_static/releases/test/index.html',
   before   => Exec['create symbolic link to current']
 }
-exec { 'create symbolic link to current'
+exec { 'create symbolic link to current':
   provider => shell,
-  command => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current'
-  before   => Exec['grant ownership']
+  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  before   => File['/data/']
 }
-exec { 'grant ownership'
-  provider => shell,
-  command => 'sudo chown -R ubuntu:ubuntu /data/'
-  before   => Exec['serve current to hbnb_static']
+file { '/data/':
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
+  before  => Exec['serve current to hbnb_static']
 }
-exec { 'serve current to hbnb_static'
+exec { 'serve current to hbnb_static':
   provider => shell,
-  command => 'sudo sed -i "51i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default'
+  command  => 'sudo sed -i "51i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default',
   before   => Exec['restart nginx']
 }
-exec { 'restart nginx'
+exec { 'restart nginx':
   provider => shell,
-  command => 'sudo service nginx restart'
+  command  => 'sudo service nginx restart'
 }
