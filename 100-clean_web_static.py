0e15dc17c1fb03ec40d2ab016dce22a7c485f8fe
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-08 09:23:53 +0100

    advanced: not a safe solution

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 '''a Fabric script deletes out-of-date archives, using the function do_clean'''
 from fabric.api import env, put, run, local
-from os.path import exists, basename, splitext
+import os
 from datetime import datetime
 env.hosts = ['52.86.164.135', '100.25.0.108']
+env.user = 'ubuntu'
 
 
 def do_clean(number=0):
     # deletes out-of-date archives
 
-    if int(number) == 0 or int(number) == 1:
-        run('ls -t | tail -n +2 | xargs rm')
+    number = int(number)
+    if number == 0 or number == 1:
+        number = 2
     else:
-        run(f'ls -t | tail -n +{number + 1} | xargs rm')
+        number += 1
+
+    local('cd versions; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {}; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
